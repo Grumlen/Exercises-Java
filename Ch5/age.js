8a20c6c4a787296cd96aee9147f112f02e6938b7
Nathan Christian <grumlen@gmail.com>
2017-01-03 15:21:46 -0600

    sequencer done

--- Ch5/age.js
+++ Ch5/age.js
@@ -44,34 +44,22 @@ function average(array) {
   return array.reduce(plus) / array.length;
 }
 // Your code here.
-var centuries = [];
-ancestry.forEach(function(person) {
-  var cent = Math.ceil(person.died/100);
-  var age = person.died-person.born;
-  centuries.push([cent,age]);
+var stuff = ancestry.map(function(person) {
+  return [Math.ceil(person.died/100),person.died-person.born];
 });
-cent16 = centuries.filter(function(x) {return x[0]==16;});
-cent17 = centuries.filter(function(x) {return x[0]==17;});
-cent18 = centuries.filter(function(x) {return x[0]==18;});
-cent19 = centuries.filter(function(x) {return x[0]==19;});
-cent20 = centuries.filter(function(x) {return x[0]==20;});
-cent21 = centuries.filter(function(x) {return x[0]==21;});
-function removeCentury(array) {
-  var popped = [];
-  for (i=0;i<array.length;i++) {
-    popped[i]=array[i].pop();
-  }
-  return popped;
+var ages = {};
+stuff.forEach(function(person) {
+  var century = person[0];
+  if (century in ages) ages[century].push(person[1]);
+  else ages[century] = [person[1]];
+});
+for (var cent in ages) {
+  console.log(cent+": "+average(ages[cent]));
 }
-console.log("16: "+average(removeCentury(cent16)));
-console.log("17: "+average(removeCentury(cent17)));
-console.log("18: "+average(removeCentury(cent18)));
-console.log("19: "+average(removeCentury(cent19)));
-console.log("20: "+average(removeCentury(cent20)));
-console.log("21: "+average(removeCentury(cent21)));
 // → 16: 43.5
 //   17: 51.2
 //   18: 52.8
 //   19: 54.8
 //   20: 84.7
+//   21: 94
 //   21: 94
--- Ch6/sequence.js
+++ Ch6/sequence.js
@@ -1,24 +1,25 @@
-// This is unfinished. I could easily create functions to do the desired tasks, but I do not know how to create the interface desired.
-"use strict";
+// Your code here.
 function logFive(array) {
-  var run=Math.min(array.length,5);
-  for (i=0;i<run;i++) {
+  for (i=0;i<Math.min(5,array.length);i++) {
     console.log(array[i]);
   }
 }
-function ArraySeq(array) {
-  for (var j=0;j<array.length;j++) {
-    this.j=array[j];
+function RangeSeq(from,to) {
+  var array = [];
+  for (i=from;i<=to;i++) {
+    array.push(i);
   }
+  return array;
 }
-console.log(new ArraySeq([1,2]));
+function ArraySeq(array) {
+  return array;
+}
 logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
-//logFive(new RangeSeq(100, 1000));
+logFive(new RangeSeq(100, 1000));
 // → 100
 // → 101
 // → 102
 // → 103
 // → 104
-// Definitely not working
