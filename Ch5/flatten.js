95f738d6ae7db1413dcfc8ea7fb48d52d8941af8
Nathan Christian <grumlen@gmail.com>
2017-01-05 10:07:55 -0600

    Ch14 2/3 done

--- Ch5/flatten.js
+++ Ch5/flatten.js
@@ -1,4 +1,11 @@
 var arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
-console.log(arrays.reduce(function(a,b) {return a.concat(b)}));
+function flatten(arr) {
+  var flat = arr.reduce(function(a,b) {return a.concat(b)},[]);
+  for (i=0;i<flat.length;i++) {
+    if (Array.isArray(flat[i])) { flat = flatten(flat);}
+  }
+  return flat;
+}
+console.log(flatten(arrays));
 // → [1, 2, 3, 4, 5, 6]
